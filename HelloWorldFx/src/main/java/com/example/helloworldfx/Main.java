ea912fcb07522cbf4729e44cd9b1108c1e71f4b6
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-19 18:04:54 -0600

    Finished HelloWorldFX project, everything works as expected.

--- HelloWorldFx/src/main/java/com/example/helloworldfx/Main.java
+++ HelloWorldFx/src/main/java/com/example/helloworldfx/Main.java
@@ -2,7 +2,13 @@ package com.example.helloworldfx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +17,17 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+//        GridPane root = new GridPane();
+//        root.setAlignment(Pos.CENTER);
+//        root.setVgap(10);
+//        root.setHgap(10);
+//        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 700, 350);
+
+//        Label greeting = new Label("Welcome to JavaFX!");
+//        greeting.setTextFill(Color.GREEN);
+//        greeting.setFont(Font.font("Times New Roman", FontWeight.BOLD, 70));
+//        root.getChildren().add(greeting);
         stage.setTitle("Hello JavaFx!");
         stage.setScene(scene);
         stage.show();
