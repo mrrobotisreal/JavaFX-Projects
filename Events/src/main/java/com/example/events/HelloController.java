cadd33ab039169b9171fb0606eacc73224d9012d
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-24 16:21:26 -0600

    Disabled buttons if nothing has been typed or if only spaces have been typed.

--- Events/src/main/java/com/example/events/HelloController.java
+++ Events/src/main/java/com/example/events/HelloController.java
@@ -15,11 +15,25 @@ public class HelloController {
     private Button byeButton;
 
     @FXML
+    public void initialize() {
+        helloButton.setDisable(true);
+        byeButton.setDisable(true);
+    }
+
+    @FXML
     public void onButtonClicked(ActionEvent e) {
         if (e.getSource().equals(helloButton)) {
             System.out.println("Hello, " + nameField.getText());
         } else if (e.getSource().equals(byeButton)) {
             System.out.println("Bye, " + nameField.getText());
         }
+    }
+
+    @FXML
+    public void onKeyReleased() {
+        String text = nameField.getText();
+        boolean disableButtons = text.isEmpty() || text.trim().isEmpty();
+        helloButton.setDisable(disableButtons);
+        byeButton.setDisable(disableButtons);
     }
 }
