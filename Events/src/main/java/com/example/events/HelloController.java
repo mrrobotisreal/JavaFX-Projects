949311ce6e0c1111d86e622e5ddca336d10f60e7
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-24 16:05:30 -0600

    Added two buttons and created if else if statement depending on which button was pressed.

--- Events/src/main/java/com/example/events/HelloController.java
+++ Events/src/main/java/com/example/events/HelloController.java
@@ -1,10 +1,25 @@
 package com.example.events;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    public void onButtonClicked() {
-        System.out.println("Hello!");
+    @FXML
+    private TextField nameField;
+    @FXML
+    private Button helloButton;
+    @FXML
+    private Button byeButton;
+
+    @FXML
+    public void onButtonClicked(ActionEvent e) {
+        if (e.getSource().equals(helloButton)) {
+            System.out.println("Hello, " + nameField.getText());
+        } else if (e.getSource().equals(byeButton)) {
+            System.out.println("Bye, " + nameField.getText());
+        }
     }
 }
