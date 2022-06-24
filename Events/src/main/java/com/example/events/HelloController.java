710b23c0052a96920ad7f19c14c14bcadae3b975
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-24 15:50:47 -0600

    Added Events project and wrote boilerplate code with a button and an event handler.

---
+++ Events/src/main/java/com/example/events/HelloController.java
@@ -0,0 +1,10 @@
+package com.example.events;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    public void onButtonClicked() {
+        System.out.println("Hello!");
+    }
+}
